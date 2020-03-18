0d35b4f40bc6c2bf5c33d2bff04d2ebae68c8080
김도형 <sdkdh168@gmail.com>
2020-03-18 22:59:35 +0900

    2nd commit // Hold for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="button">BUTTON</button>
+      </div>
+    )
+  }
 }
 
 export default App;
